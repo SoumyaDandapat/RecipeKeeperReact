001a7a23be70600e2696facfd13742208f00cf1a
Soumya Soubhagya Dandapat <soumyasoubhagyadandapat@gmail.com>
2022-08-30 08:54:06 +0530

    basic search bar added

--- assets/screens/RecipeListScreen.js
+++ assets/screens/RecipeListScreen.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RECIPES } from "../data/mockdata";
-import { View, FlatList, StyleSheet, Text } from "react-native";
+import { View, FlatList, StyleSheet, TextInput } from "react-native";
 import RecipeTile from '../components/RecipeTile';
 
 
 function RecipeListScreen({ navigation }) {
+
+    const [filteredData, setFilteredData] = useState([]);
+    const [masterData, setMasterData] = useState([]);
+    const [search, setSearch] = useState('');   
+
+    useEffect(() =>{
+        fetchData();
+        return () =>{}
+    },[])
+
+    const fetchData = () =>{
+        setFilteredData(RECIPES)
+        setMasterData(RECIPES)
+    }
+
+    const searchFilter = (text) =>{
+         if(text){
+            const newData = masterData.filter((item)=>{
+                const itemData = item.name? item.name.toUpperCase():''.toUpperCase()
+                const textData = text.toUpperCase()
+                return itemData.indexOf(textData)>-1;
+            });
+            setFilteredData(newData);
+            setSearch(text);
+         }
+         else{
+            setFilteredData(masterData);
+            setSearch(text)
+         }
+    }
+
     function returnRecipe(itemData) {
         function onPress() {
             navigation.navigate('RecipeDetailScreen', {
@@ -19,8 +50,16 @@ function RecipeListScreen({ navigation }) {
 
     return (
         <View style={styles.viewContainer}>
+            <TextInput
+                style = {styles.textInputStyle}
+                value = {search}
+                placeholder = "Search"
+                underlineColorAndroid="transparent"
+                onChangeText = {(text)=>searchFilter(text)}
+            />
             <FlatList
-                data={RECIPES}
+                style = {styles.flatListStyle}
+                data={filteredData}
                 keyExtractor={(item) => item.id}
                 renderItem={returnRecipe}
                 numColumns={2}
@@ -30,10 +69,20 @@ function RecipeListScreen({ navigation }) {
 
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ 
     viewContainer: {
         marginTop: 16,
         width: '100%'
+    },
+    textInputStyle: {
+        height: 50,
+        borderWidth: 1,
+        margin: 5,
+        borderColor: '#009688',
+        backgroundColor: 'white'
+    },
+    flatListStyle:{
+        marginBottom: 60
     }
 })
 
