62bab62d0923ccf76ba62c879b4649df68800ead
Soumya Soubhagya Dandapat <soumyasoubhagyadandapat@gmail.com>
2022-09-02 15:12:45 +0530

    added favourite button in card

---
+++ assets/components/IconButton.js
@@ -0,0 +1,13 @@
+import { Pressable } from "react-native";
+import React from "react";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+function IconButton({ onPress, name, size, color }) {
+    return <Pressable onPress={onPress}>
+        <Ionicons name={name} size={size} color={color} />
+    </Pressable>
+}
+
+
+
+export default IconButton
