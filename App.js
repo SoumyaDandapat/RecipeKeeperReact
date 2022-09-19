54448065d2b0f44913e86ac0cbd10d2c0ed11bda
Soumya Soubhagya Dandapat <soumyasoubhagyadandapat@gmail.com>
2022-09-19 11:13:05 +0530

    added back handler and alert

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RecipeListScreen from './assets/screens/RecipeListScreen';
 import RecipeDetailScreen from './assets/screens/RecipeDetailScreen';
@@ -11,6 +11,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Provider } from 'react-redux';
 import { store, persistor } from './assets/redux/store';
 import { PersistGate } from 'redux-persist/integration/react'
+import { Alert, BackHandler } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -59,6 +60,31 @@ function DrawerNavigator() {
 
 
 export default function App() {
+
+  useEffect(()=>{
+    const backAction = () =>{
+      Alert.alert("Exit","Do you want to close the App?",[
+        {
+          text:"No",
+          onPress:()=> null,
+          style:"cancel"
+        },
+        {
+          text:"Yes",
+          onPress:()=>BackHandler.exitApp()
+        }
+      ])
+      return true
+    }
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    )
+
+    return () => backHandler.remove()
+  })
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
